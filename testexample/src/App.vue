2111f38adeae1edddc0c0b978554accc223ad345
hellomrbigshot <1994yechen@gmail.com>
2021-11-29 21:45:51 +0800

    ✨

---
+++ testexample/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Vue3Component/>
+  <input ref="inputRef"/>
+</template>
+
+<script>
+import { defineComponent, onMounted, nextTick, ref } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  setup () {
+    const inputRef = ref()
+    onMounted(() => {
+      console.log('example mounted')
+      nextTick(() => {
+        console.log('inputRef', inputRef.value)
+      })
+    })
+    return {
+      inputRef
+    }
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ testexample/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { Vue3Component } from 'vue3-component'
+
+
+createApp(App).use(Vue3Component).mount('#app')
